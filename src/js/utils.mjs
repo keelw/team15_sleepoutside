45370caeb1bd5938b7101c33db65369f52307f91
Yoli-Buendia <bue17003@byui.edu>
2024-01-16 19:23:36 -0600

    individual activity - Add Item to Cart Broken

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,10 +9,25 @@ export function qs(selector, parent = document) {
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
 // save data to local storage
+//export function setLocalStorage(key, data) {
+  //localStorage.setItem(key, JSON.stringify(data));
+//}
+
+//Fixing storage
+//make the local storage to store array instead of objects
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  
+  //create an array to store getLocalStorage(function that stores the items that are selected one by one)
+  //storing an obect(getLocalStorage function) into an array(variable array)
+  let array = getLocalStorage(key);
+  //to add each item into the array[]
+  array.push(data);
+  //return the array
+  localStorage.setItem(key, JSON.stringify(array)); 
 }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
