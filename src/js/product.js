e86d9a6fc027153c1c090d8a40739a958b2798a9
Christopher Cortes <christopherctes@hotmail.com>
2024-01-20 14:51:32 -0300

    add more product to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
+// Initialize a variable with the current length of the local storage.
+// It is intended to be used as a unique idenfifier for items in the shopping cart. 
+let storeItem = localStorage.length;
+
+//After storing the product, it increments 'storeItem'. It is meant to add a product to the shopping cart.
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  setLocalStorage('so-cart' + storeItem, product);
+  storeItem++;
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -14,5 +21,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
