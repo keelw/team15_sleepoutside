46678fe3a4dd467b1764d9fee3dcd7db04f612bd
Erik Burton <erikburton1@gmail.com>
2024-01-19 21:00:08 -0500

    Changed ' to " = npm run format

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import ProductData from './ProductData.mjs';
+import ProductData from "./ProductData.mjs";
 
-import ProductList from './ProductList.mjs';
+import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
