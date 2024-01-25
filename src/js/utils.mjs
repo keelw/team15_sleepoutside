372a352d0591ec09c07a358f67d89c479033fc87
keelw <wjordankeel@gmail.com>
2024-01-24 17:51:33 -0700

    formatting utils.mjs since team debugging

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,67 +2,68 @@
   export function qs(selector, parent = document) {
     return parent.querySelector(selector);
   }
+
   // or a more concise version if you are into that sort of thing:
   // export const qs = (selector, parent = document) => parent.querySelector(selector);
   // retrieve data from localstorage
   export function getLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key));
   }
+
   // save data to local storage
     export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+    localStorage.setItem(key, JSON.stringify(data));
   }
+
   // set a listener for both touchend and click
   export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-  event.preventDefault();
-  callback();
-  });
-  qs(selector).addEventListener("click", callback);
+    qs(selector).addEventListener("touchend", (event) => {
+      event.preventDefault();
+      callback();
+    });
+    qs(selector).addEventListener("click", callback);
   }
+
   export function getParam(productId) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product')
-  return product;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const product = urlParams.get('product')
+    return product;
   }
+
   // render a list with the provided template 
-  export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
-  const htmlStrings = list.map(templateFn);
-  if (clear) {
-  parentElement.innerHTML = "";
+    export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+    const htmlStrings = list.map(templateFn);
+    if (clear) {
+      parentElement.innerHTML = "";
+    }
+    parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
   }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-  }
-  // // render an item with the provided template
-  // export function renderWithTemplate(template, parent, data, callback) {
-  //   parent.insertAdjacentHTML("afterbegin", template);
-  //   if(callback) {
-  //     callback(data);
-  //   }
-  // }
+
   // render an item with the provided template
   export function renderWithTemplate(template, parent, data, callback) {
-  parent.insertAdjacentHTML("afterbegin", template);
-  if (callback) {
-  callback(data);
+    parent.insertAdjacentHTML("afterbegin", template);
+    if (callback) {
+      callback(data);
+    }
   }
+
+  export async function loadHeaderFooter () {
+    const header = document.getElementById("main-header");
+    const footer = document.getElementById("footer");
+    const headerTemplate = await loadTemplate("../public/partials/header.html");
+    const footerTemplate = await loadTemplate("../public/partials/footer.html");
+    renderWithTemplate(headerTemplate, header);
+    renderWithTemplate(footerTemplate, footer);
   }
-  export async function loadHeaderFooter ()
-  {
-  const header = document.getElementById("main-header");
-  const footer = document.getElementById("footer");
-  const headerTemplate = await loadTemplate("../public/partials/header.html");
-  const footerTemplate = await loadTemplate("../public/partials/footer.html");
-  renderWithTemplate(headerTemplate, header);
-  renderWithTemplate(footerTemplate, footer);
-  }
+
   async function loadTemplate(path) {
-  const res = await fetch(path);
-  const template = await res.text();
-  return template;
+    const res = await fetch(path);
+    const template = await res.text();
+    return template;
   }
+
   // calculate the percentage off for the sale price display
   export function calculatePercentage(fullPrice, listPrice) {
-  return Math.round(100 - (fullPrice / listPrice * 100 * 10) / 10);
+    return Math.round(100 - (fullPrice / listPrice * 100 * 10) / 10);
   }
