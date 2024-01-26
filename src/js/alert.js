0be326726a0844440c3934c95a40b1cd42b3fce7
Erik Burton <erikburton1@gmail.com>
2024-01-26 18:02:50 -0500

    Refactors alert.js to read JSON file

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,10 +1,19 @@
 class Alert {
-    constructor(message) {
+    constructor(message, background, color) {
         this.message = message;
+        this.background = background;
+        this.color = color;
     }
 
     showMessage() {
-        alert(this.message);
+        const alertContainer = document.createElement('div');
+        alertContainer.style.backgroundColor = this.background;
+        alertContainer.style.color = this.color;
+        alertContainer.style.padding = '10px';
+        alertContainer.style.margin = '10px';
+        alertContainer.textContent = this.message;
+        
+        document.body.appendChild(alertContainer);
     }
 }
 
