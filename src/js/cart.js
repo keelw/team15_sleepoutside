9e713e4ee5491057ff38fcacaf128df405411f23
keelw <wjordankeel@gmail.com>
2024-01-27 20:17:07 -0700

    formatting

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
